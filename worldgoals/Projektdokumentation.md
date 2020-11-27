3e7be5fb2228bdc161757dac53dc0ffac9b8f707
kemex10 <61971140+kemex10@users.noreply.github.com>
2020-11-27 09:48:27 +0100

    Add files via upload

---
+++ worldgoals/Projektdokumentation.md
@@ -0,0 +1,6 @@
+jeg har Arrow functions i stedet for nogle extre funktioner så det er en nemmere kodeskrivning 
+Closures bruger jeg fordi jeg har nogle funktioner der høre sammen så de er skrevet i den samme funtion 
+Rest parameter bruger jeg for at kunne skrive mere i samme array 
+Spread operator bliver brugt for at kunne forsætte uden at skulle skrive hele ordet 
+Destructuring Assignment bruges til at udpakke værdier fra arrays eller egenskaber fra objekter til forskellige variabler.
+har ikke brugt Error catching
---
+++ worldgoals/js/app.js
@@ -0,0 +1,86 @@
+const url = "https://api.mediehuset.net/sdg/goals";
+const maincontainer = document.getElementById('main');
+
+function ListController () {
+    removeHTML();    
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            for(let item of data.items) {
+                const {id, title, icon: svg, byline, color} = item;
+                ListView(id, title, byline, color, svg);
+            }
+        })
+}
+
+function ListView (id, title, byline, color, svg) {
+    const div = document.createElement('div');
+    div.setAttribute('id', `goal-${id}`);
+    div.style.backgroundColor = `#${color}`;
+    div.innerHTML = svg;
+
+    const h1 = document.createElement('h1');
+    h1.innerText = title;
+    div.appendChild(h1);
+
+    const span_byline = document.createElement('span');
+    span_byline.innerText = byline;
+    div.appendChild(span_byline);
+
+    div.onclick = () => {
+        DetailController(id);
+    }
+
+    maincontainer.appendChild(div);
+
+}
+
+function DetailController(id) {
+
+    fetch(`${url}/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            removeHTML();
+            DetailView(data.item);
+        })
+}
+
+function DetailView({id, title, description, image, color } = data) {
+    const div = document.createElement('div');
+    div.style.backgroundColor = `#${color}`;
+
+    const h1 = document.createElement('h1');
+    h1.innerText = title;
+    div.appendChild(h1);
+
+    const a = document.createElement('a');
+    a.innerHTML = '&laquo; Tilbage til oversigt';
+    a.onclick = () => {
+        ListController();
+    }
+    div.appendChild(a);
+
+    const img = document.createElement('img');
+    img.src = image;
+    div.appendChild(img);
+
+    const p = document.createElement('p');
+    p.innerText = description;
+    div.appendChild(p);
+
+    maincontainer.appendChild(div);    
+
+}
+
+function removeHTML() {
+    maincontainer.innerHTML = '';
+}
+
+ListController();
+
+function goBack() {
+
+    window.history.back();
+  
+    }
